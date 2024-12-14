855917733287086adda1f996413968145a33c368
Darshana Perera <ds.perera.1997@gmail.com>
2024-12-14 12:13:41 +0530

    initial pages created

---
+++ frontend/src/Design/Design.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Pages/Home";
+import Form from "./Pages/Form";
+import Application from "./Pages/Application";
+
+export default function Design() {
+  return (
+    <div>
+      <Home />
+      <Form />
+      <Application />
+    </div>
+  );
+}
