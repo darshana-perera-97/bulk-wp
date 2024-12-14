0d8129313733e1ea44f001456b06b35f457409ee
Darshana Perera <ds.perera.1997@gmail.com>
2024-12-14 12:04:07 +0530

    otp generated

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,16 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-// Example API route
+// Example API route to receive data
 app.post("/api/data", (req, res) => {
   const data = req.body;
   res.send({ message: "Data received!", data });
+});
+
+// API to generate a random 6-digit OTP
+app.get("/otp", (req, res) => {
+  const otp = Math.floor(100000 + Math.random() * 900000); // Generates a random 6-digit number
+  res.send({ otp });
 });
 
 // Start the server
